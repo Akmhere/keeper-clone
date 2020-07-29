87e559bc5cc54df81e793bb1186d2ee0a2df59b4
Akmhere <itzakm33@gmail.com>
2020-07-29 11:05:20 +0530

    Updates

---
+++ precache-manifest.f4efad80e0e7706a522bead01c6f8caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bf03b59ed96beb85186af2c4849236c",
+    "url": "/keeper-clone/index.html"
+  },
+  {
+    "revision": "c01e8595b36eb8d15f85",
+    "url": "/keeper-clone/static/js/2.994172df.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/keeper-clone/static/js/2.994172df.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "576849a96cbf78fb13ad",
+    "url": "/keeper-clone/static/js/main.9f0cfb86.chunk.js"
+  },
+  {
+    "revision": "ab686bbbdb5ff2b0345d",
+    "url": "/keeper-clone/static/js/runtime-main.b0c435d4.js"
+  }
+]);
